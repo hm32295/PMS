eeb244e0cc8b3a869dff5c4a92a96b9264a00981
hm32295 <hm32295@gmail.com>
2025-06-17 12:46:43 +0300

    update context file

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState, createContext } from "react";
+
+import {jwtDecode} from "jwt-decode";
+
+export const AuthContext:any = createContext();
+
+
+export default function AuthContextProvider({ children }) {
+
+    const [loginData, setLoginData] = useState(null);
+    const [isAuthLoading, setIsAuthLoading] = useState(true);
+  
+    const saveLoginData = () => {
+      try {
+        const encodedToken = localStorage.getItem("token");
+        if (!encodedToken) return;
+  
+        const decodedToken:any = jwtDecode(encodedToken);
+        setLoginData(decodedToken);
+        setIsAuthLoading(false);
+      } catch (error) {
+        localStorage.removeItem("token");
+        setLoginData(null);
+        setIsAuthLoading(false)
+       
+      }
+    };
+  
+    const logout = () => {
+      localStorage.removeItem("token");
+      setLoginData(null);
+     
+    };
+  
+    useEffect(() => {
+      if (localStorage.getItem("token")) {
+        saveLoginData();
+      }else{
+        setIsAuthLoading(false)
+      }
+    }, []);
+  
+    
+    
+    return (
+      <AuthContext.Provider value={{ saveLoginData, loginData, logout,isAuthLoading}} >
+        
+        {children}
+
+      </AuthContext.Provider>
+    );
+  }
