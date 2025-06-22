885be0882e84389a4e60623725cfcf6e18f50914
YOUSEF <ghhjhkkoooppppplll@gmail.com>
2025-06-22 06:03:23 +0300

    NotFound &&NoData

--- src/modules/Shared/componetns/NotFound/NotFound.tsx
+++ src/modules/Shared/componetns/NotFound/NotFound.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './notfound.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+  let location=useLocation()
+ let funHome=()=>{
+    navi('/dashboard')
+  }
+  let navi=useNavigate()
   return (
-    <div>NotFound</div>
-  )
+    <div className="not-found-container">
+      <div className="error-content">
+        <h1 className="error-number">404</h1>
+        <p className="error-message">Page :<span > {location.pathname}</span> Not Found </p>
+        <button className='Dashboard_Unfound mt-2'onClick={funHome}>Dashboard</button>
+      </div>
+    </div>
+  );
 }
