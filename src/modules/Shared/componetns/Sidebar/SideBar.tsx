d3015189e5b4d89b47150aaae5fa5e7b9048129a
hm32295 <hm32295@gmail.com>
2025-06-28 16:27:05 +0300

    handel sidebar

--- src/modules/Shared/componetns/Sidebar/SideBar.tsx
+++ src/modules/Shared/componetns/Sidebar/SideBar.tsx
@@ -1,43 +1,41 @@
-import React, { useContext, useState } from 'react';
+import  { useContext, useState } from 'react';
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import './sidepar.css';
 import { IoHome } from 'react-icons/io5';
 import { HiOutlineUsers } from 'react-icons/hi2';
 import { FiGrid } from 'react-icons/fi';
-import { LuCalendarSync } from 'react-icons/lu';
-import { FaUnlockAlt } from 'react-icons/fa';
 import { HiLogout } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom';
 import { FaTasks } from "react-icons/fa";
-import { LogOut } from 'react-feather';
+// import { LogOut } from 'react-feather';
 import { AuthContext } from '../../../../context/AuthContext';
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
 const Sidepar = () => {
   const [collapsed, setCollapsed] = useState(true);
-  let{logout, loginData} = useContext(AuthContext);
+  let{logout, loginData}:{logout:any,loginData:any} = useContext(AuthContext);
   
 
   const toggleSidebar = () => {
     setCollapsed(prev => !prev);
   };
-  let navi=useNavigate();
+  let navigation=useNavigate();
  
   let funHome=()=>{
-    navi('/dashboard')
+    navigation('/dashboard')
   }
    let funUsers=()=>{
-    navi('/dashboard/users')
+    navigation('/dashboard/users')
   }
-   let funPrjects=()=>{
-    navi('/dashboard/project-List')
+   let funProject=()=>{
+    navigation('/dashboard/project-List')
   }
    let funTasks=()=>{
-    navi('/dashboard/tasks-list')
+    navigation('/dashboard/tasks-list')
   }
    let funTasksBoard=()=>{
-    navi('/dashboard/tasks-board')
+    navigation('/dashboard/tasks-board')
   }
   
   return (
@@ -46,33 +44,36 @@ const Sidepar = () => {
       className={`sidebar-root ${collapsed ? 'is-collapsed' : ''}`}
       collapsed={collapsed}
     >
-      <div className="sidebar-header" onClick={toggleSidebar} style={{ cursor: 'pointer' }}>
-    <h3> {collapsed ? <MdKeyboardDoubleArrowRight size={50} /> : <MdKeyboardDoubleArrowLeft size={50} />}</h3>   
-      </div>
+      <div className={`fixed position-fixed top-0 start-0 ${collapsed ? "w_Fixed_80" : "w_Fixed_250"}`}>
 
-     <div className="mt-3">
-  <Menu iconShape="circle" className={`sidebar-TopItems ${collapsed ? 'sidebar-TopItems-is-collapsed' : ''}`}>
-        <MenuItem icon={<IoHome />} onClick={funHome} className="menu-item-custom">Home</MenuItem>
-        {loginData?.userGroup === "Manager" &&
-          <MenuItem icon={<HiOutlineUsers />} onClick={funUsers} className="menu-item-custom">Users</MenuItem>
-        }
-        <MenuItem icon={<FiGrid />} className="menu-item-custom"  onClick={funPrjects}>Projects</MenuItem>
-        {loginData?.userGroup === "Manager" &&
-          <MenuItem icon={<FaTasks />} className="menu-item-custom" onClick={funTasks}>Tasks</MenuItem>
-        }
-        {loginData?.userGroup !== "Manager" &&
-          <MenuItem icon={<FaTasks />} className="menu-item-custom" onClick={funTasksBoard}>Tasks</MenuItem>
-        }
-      </Menu>
-         <div>
-        <Menu iconShape="circle" >
-       
-          <MenuItem icon={<HiLogout />}onClick={()=> {logout();navi("/")}} className="menu-item-custom">Logout</MenuItem>
-        </Menu>
-      </div>
+     
+            <div className="sidebar-header" onClick={toggleSidebar} style={{ cursor: 'pointer' }}>
+          <h3> {collapsed ? <MdKeyboardDoubleArrowRight size={50} /> : <MdKeyboardDoubleArrowLeft size={50} />}</h3>   
+            </div>
+
+          <div className="mt-3">
+        <Menu iconShape="circle" className={`sidebar-TopItems ${collapsed ? 'sidebar-TopItems-is-collapsed' : ''}`}>
+              <MenuItem icon={<IoHome />} onClick={funHome} className="menu-item-custom">Home</MenuItem>
+              {loginData?.userGroup === "Manager" &&
+                <MenuItem icon={<HiOutlineUsers />} onClick={funUsers} className="menu-item-custom">Users</MenuItem>
+              }
+              <MenuItem icon={<FiGrid />} className="menu-item-custom"  onClick={funProject}>Projects</MenuItem>
+              {loginData?.userGroup === "Manager" &&
+                <MenuItem icon={<FaTasks />} className="menu-item-custom" onClick={funTasks}>Tasks</MenuItem>
+              }
+              {loginData?.userGroup !== "Manager" &&
+                <MenuItem icon={<FaTasks />} className="menu-item-custom" onClick={funTasksBoard}>Tasks</MenuItem>
+              }
+            </Menu>
+              <div>
+              <Menu iconShape="circle" >
+            
+                <MenuItem icon={<HiLogout />}onClick={()=> {logout();navigation("/")}} className="menu-item-custom">Logout</MenuItem>
+              </Menu>
+            </div>
+          </div>
+          
      </div>
-    
-   
     </Sidebar>
   );
 };
